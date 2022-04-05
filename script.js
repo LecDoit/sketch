5ab820512c6c4e40c7f754002a12099bdf740c85
pikimaru <pikajunglist@gmail.com>
2022-04-05 22:44:56 +0200

    Created some awesome stuff

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const containter = document.querySelector('.test');
-console.log(containter)
 
-function makeRows(rows,cols){
-    containter.style.setProperty("--grid-rows",rows);
-    containter.style.setProperty("--grid-cols",cols);
+function makeGrid(x,y){
+    let tbl = document.querySelector('.test');
 
-    for (c=0; c<(rows*cols);c++){
-        let cell = document.createElement('div');
-        cell.innerText = (c + 1);
-        containter.appendChild(cell).className = "grid-item"
-    };
+    for (let i=0; i<x;i++){
+        let myRow = document.createElement("div");
+        myRow.id = ("row" +i);
+  
+        tbl.appendChild(myRow);      
+        let rowW = document.getElementById("row"+i);
+
+        myRow.classList.add('rowie');
+
+        for(let j=0; j<y; j++){
+            let myCell = document.createElement("div");
+            myCell.id = ('nice')
+            rowW.appendChild(myCell);
+
+        }
+    }
 };
 
-makeRows(8,8);
+// makeGrid(16,16);
+
+let test = document.getElementById('div')
+
+test.addEventListener('click',function(event){
+event.target.classList.toggle('collored')
+})
+
+let btn = document.getElementById('btn')
+
+
+btn.addEventListener('click',function(event){
+    var x = prompt("Please provide number of Rows");
+    var y = prompt("Please provide number of Columns");
+    makeGrid(x,y)
+})
